4bd0d2bfb561ba48672162d89372e7821c33d1b5
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2019-07-23 18:49:54 -0300

    Alterado para o preset do react

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,15 @@
 /**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require('./bootstrap');
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+require('./components/Example');
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Barato Coletivo</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<h1>index</h1>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
