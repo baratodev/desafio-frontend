5d7aefacd8612d45b5ac950ceac4e05157cd9fce
clodoaldo dantas <clodoaldodantas8@gmail.com>
2019-07-23 14:44:01 -0300

    new component: vue-ionic-icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <router-view />
+    <MdBeerIcon />
   </div>
 </template>
 
 <style lang="scss">
+@import "~vue-ionicons/ionicons.scss";
 @import "@/assets/reset.scss";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home">
     <h1>Vue</h1>
+    <MdBeerIcon class="icon" />
   </div>
 </template>
 
 <script>
+export default {
+  name: "home"
+};
 </script>
+
+
