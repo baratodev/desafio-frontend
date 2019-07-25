fc272235ba2de40b097175eb4bf1c3e440f0848e
clodoaldo dantas <clodoaldodantas8@gmail.com>
2019-07-25 13:11:45 -0300

    layout changes

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -9,7 +9,9 @@
           <input type="text" placeholder="validade" />
           <input type="text" placeholder="codigo de seg." />
         </div>
-        <button class="btn">Pagar</button>
+        <button class="btn">
+          <MdCashIcon />Pagar
+        </button>
       </form>
       <div>
         <Heading text="Resumo da Compra" />
@@ -20,55 +22,18 @@
 
 <script>
 import Heading from "@/components/Heading";
+import MdCashIcon from "vue-ionicons/dist/md-cash.vue";
 
 export default {
   name: "cart",
   components: {
-    Heading
+    Heading,
+    MdCashIcon
   }
 };
 </script>
 
 
 <style lang="scss" scoped>
-.cart {
-  margin-top: 40px;
-  margin-bottom: 40px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 30px;
-
-  &__form {
-    border-right: 1px solid var(--color-primary);
-    padding-right: 40px;
-    height: calc(100vh - 200px);
-
-    .title {
-      margin-bottom: 30px;
-    }
-
-    input {
-      font-family: "Quicksand", sans-serif;
-      color: var(--color-body);
-      font-size: 1rem;
-      padding: 15px;
-      display: block;
-      width: 100%;
-      border-radius: 10px;
-      border: 1px solid var(--color-input);
-      margin-bottom: 15px;
-      outline: none;
-    }
-
-    .input-group {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      grid-gap: 20px;
-    }
-
-    .btn {
-      margin-top: 45px;
-    }
-  }
-}
+@import "@/assets/cart.scss";
 </style>
