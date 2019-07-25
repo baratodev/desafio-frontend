6c019a0523010775b6056fc87bddc868c66c63df
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-25 02:00:44 -0300

    Arquivos de setup do Vue

---
+++ src/material.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+import './assets/scss/themes/light.scss';
+import './assets/scss/themes/dark.scss';
---
+++ src/vue-setup.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+
+import './material';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
