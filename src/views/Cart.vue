0ca2bf15899d32a59013806a9612555b55d99d23
clodoaldo dantas <clodoaldodantas8@gmail.com>
2019-07-25 11:21:31 -0300

    new page: Cart

---
+++ src/views/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="cart">
+      <h1>Cart</h1>
+      <h1>total</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cart {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 30px;
+}
+</style>
