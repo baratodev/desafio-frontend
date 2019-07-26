09749a1ac89c591e052de91bf3883c10fdcc50de
Pedro Alves <pedro.alves@bannet.com.br>
2019-07-26 13:15:44 -0300

    Componente para renderização do card da oferta

--- src/components/Ofertas/Item.vue
+++ src/components/Ofertas/Item.vue
@@ -1,13 +1,47 @@
 <template>
-  
+  <md-card>
+    <md-card-area>
+      <md-card-media>
+        <img :src="$props.oferta.image.url" :alt="$props.oferta.title" class="md-image">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">Actions left aligned</div>
+        <div class="md-subhead">Subtitle here</div>
+      </md-card-header>
+
+      <md-card-content>
+        {{ $props.oferta.title }}
+      </md-card-content>
+    </md-card-area>
+
+    <md-card-actions md-alignment="left">
+      <md-button class="md-accent" v-on:click="adicionarAoCarrinho($props.oferta)">
+        Adicionar ao carrinho
+        <md-icon>shopping_cart</md-icon>
+      </md-button>
+    </md-card-actions>
+  </md-card>
 </template>
 
 <script>
+  import {mapMutations} from 'vuex';
+  import {types} from '../../store/mutations';
+
   export default {
-    name: "Item"
+    name: "Item",
+    props: {
+      oferta: {
+        title: null,
+        image: {
+          url: null
+        }
+      }
+    },
+    methods: {
+      ...mapMutations([
+        types.ADICIONAR_AO_CARRINHO
+      ])
+    }
   }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ import Layout from './Layout.vue';
 
 /* Ofertas */
 import ListaOfertas from './Ofertas/Lista.vue';
+import ItemOferta from './Ofertas/Item.vue';
 
 Vue.component('bc-layout', Layout);
 Vue.component('bc-lista-ofertas', ListaOfertas);
+Vue.component('bc-item-oferta', ItemOferta);
