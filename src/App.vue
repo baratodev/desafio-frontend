deb33820242205dcfd10cfc838352fcfec89adb9
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-26 00:55:50 -0300

    Atualizando tema de acordo com o estado no store

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
+  import Vue from 'vue';
+  import {mapGetters} from 'vuex';
+
   export default {
-    name: "App"
+    name: "App",
+    computed: {
+      ...mapGetters([
+        'temaEscuro'
+      ])
+    },
+    watch: {
+      temaEscuro: {
+        immediate: true,
+        handler() {
+          Vue.material.theming.theme = this.temaEscuro ? 'bc-dark' : 'bc';
+        }
+      }
+    }
   }
 </script>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import Layout from './Layout.vue';
+
+Vue.component('bc-layout', Layout);
