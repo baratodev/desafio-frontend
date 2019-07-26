1bcc635bac159f75f56fb910bafbfd04c746e590
Pedro Alves <pedro.alves@bannet.com.br>
2019-07-26 12:44:24 -0300

    Helper para manipular o carrinho

---
+++ src/helpers/carrinho.js
@@ -0,0 +1,23 @@
+import storage from "./storage";
+
+const listar = () => storage.pegar('carrinho') || [];
+
+const adicionar = (oferta) => {
+  const carrinho = listar();
+  carrinho.push(oferta);
+  storage.inserir('carrinho', carrinho);
+  return carrinho;
+};
+
+const remove = (indice) => {
+  const carrinho = listar();
+  carrinho.splice(indice, 1);
+  storage.inserir('carrinho', carrinho);
+  return carrinho;
+};
+
+export default {
+  listar,
+  adicionar,
+  remove
+};
