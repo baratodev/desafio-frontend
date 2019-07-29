8d08c4340fb94774ee4ae598d244d925f16c754a
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-29 01:09:08 -0300

    Transformando o objeto em string antes de inserir no localStorage

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,5 +1,5 @@
 const pegar = (item) => JSON.parse(localStorage.getItem(item));
-const inserir = (item, valor) => localStorage.setItem(item, valor);
+const inserir = (item, valor) => localStorage.setItem(item, JSON.stringify(valor));
 
 export default {
   pegar,
