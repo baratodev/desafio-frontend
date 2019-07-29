b80d8e93bc08708ce1b4c57efa50755b485a3368
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-29 03:19:00 -0300

    Adicionando vue-the-mask nas dependências do projeto

--- src/vue-setup.js
+++ src/vue-setup.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
+import VueTheMask from 'vue-the-mask';
 
 import './material';
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VueTheMask);
