a4264aefde6e87658595f95f145f6b17477c9f6f
Pedro Alves <pedro.alves@bannet.com.br>
2019-07-29 12:58:44 -0300

    Funções para listar itens únicos e para limpar o carrinho adicionados

--- src/helpers/carrinho.js
+++ src/helpers/carrinho.js
@@ -2,6 +2,16 @@ import storage from "./storage";
 
 const listar = () => storage.pegar('carrinho') || [];
 
+const listarUnicos = () => {
+  const carrinho = listar();
+  const itemsUnicos = carrinho.reduce((obj, item) => {
+    obj[item.id] = { ...item };
+
+    return obj;
+  }, {});
+  return Object.values(itemsUnicos);
+};
+
 const adicionar = (oferta) => {
   const carrinho = listar();
   carrinho.push(oferta);
@@ -16,8 +26,16 @@ const remover = (indice) => {
   return carrinho;
 };
 
+const limpar = () => {
+  const carrinho = [];
+  storage.inserir('carrinho', carrinho);
+  return carrinho;
+};
+
 export default {
   listar,
+  listarUnicos,
   adicionar,
-  remover
+  remover,
+  limpar
 };
