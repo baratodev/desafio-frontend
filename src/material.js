f29ce9d417b6fe9e620164150f15dfeeedb1fe38
Pedro Alves <pedro.alves@bannet.com.br>
2019-07-29 18:04:45 -0300

    Voltando configuração do tema da aplicação

--- src/material.js
+++ src/material.js
@@ -1,37 +1,8 @@
 import Vue from 'vue';
-
-import {
-  MdApp,
-  MdToolbar,
-  MdButton,
-  MdField,
-  MdIcon,
-  MdDivider,
-  MdList,
-  MdLayout,
-  MdBadge,
-  MdCard,
-  MdContent,
-  MdMenu,
-  MdSwitch,
-  MdSnackbar
-} from 'vue-material/src/components';
+import VueMaterial from 'vue-material';
 
 import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/vue-material.min.css';
-import './assets/scss/themes/index.scss';
+import './assets/scss/themes/dark.scss';
+import './assets/scss/themes/light.scss';
 
-Vue.use(MdApp);
-Vue.use(MdToolbar);
-Vue.use(MdButton);
-Vue.use(MdField);
-Vue.use(MdIcon);
-Vue.use(MdDivider);
-Vue.use(MdList);
-Vue.use(MdLayout);
-Vue.use(MdBadge);
-Vue.use(MdCard);
-Vue.use(MdContent);
-Vue.use(MdMenu);
-Vue.use(MdSwitch);
-Vue.use(MdSnackbar);
+Vue.use(VueMaterial);
